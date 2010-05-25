cfb67582468a4310bdef70281fe7953a570ed924
Tomas Barton <barton.tomas@gmail.com>
2010-05-25 17:35:36 +0200

    added new semantics errors

--- code/semantic.cc
+++ code/semantic.cc
@@ -105,6 +105,27 @@ void errorfielddefined(int l, string s) {
   cout<<"L. "<<l<<": Field "<<s<<" already defined in the struct."<<endl;
 }
 
+void errornumberitemsunpack(int l) {
+  TypeError = 1;
+  cout<<"L. "<<l<<": The number of items in the unpacking assignment do not match."<<endl;
+}
+
+void errornonreferenceableleftunpack(int l, int n) {
+  TypeError = 1;
+  cout<<"L. "<<l<<": Left expression of item " << n << " in unpacking assignment is not referenceable."<<endl;
+}
+
+void errorincompatibleassignmentunpack(int l, int n) {
+  TypeError = 1;
+  cout<<"L. "<<l<<": Unpacking assignment with incompatible types for item " << n << "." << endl;
+}
+
+void errorunpackrequiresstruct(int l) {
+  TypeError = 1;
+  cout<<"L. "<<l<<": Unpacking assignment requires a struct." << endl;
+}
+
+
 /// ------------------------------------------------------------
 /// Table to store information about program identifiers
 symtab symboltable;
